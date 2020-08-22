4b917ee32983e473369b23f9356a99bca68a982f
descholar-ceo <emmanuellamugi@gmail.com>
2020-08-22 15:31:09 +0200

    Add welcome message

---
+++ install.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Welcome to the Bo!ler cli utility, We will initialize your basic project, \nbut to do so, you will help us with few answers to the following questions.")
+}
