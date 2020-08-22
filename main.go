4fd12097d49cffac6dc6412132ffddbc94195888
descholar-ceo <emmanuellamugi@gmail.com>
2020-08-22 19:58:50 +0200

    Addinitialize spec

--- main.go
+++ main.go
@@ -89,7 +89,7 @@ func main() {
 	// initialize rspec
 	fmt.Printf("Initializing rspec in %s directory...\n", projectName)
 	os.Mkdir(projectName+"/spec", 0755)
-	copy("./lib/.ruby/spec", projectName+"/spec/spec_helper.rb")
+	copy("./lib/.ruby/spec/spec_helper.rb", projectName+"/spec/spec_helper.rb")
 	copy("./lib/.ruby/.rspec", projectName+"/.rspec")
 
 }
