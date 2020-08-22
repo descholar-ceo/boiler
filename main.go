8a9deae09d954c8fb99c35d5922989fade126059
descholar-ceo <emmanuellamugi@gmail.com>
2020-08-22 18:03:04 +0200

    Add basic logic of questions

--- main.go
+++ main.go
@@ -2,20 +2,24 @@ package main
 
 import (
 	"fmt"
-	"os"
 )
 
 func main() {
 	fmt.Println("Welcome to the Bo!ler cli utility, We will initialize your basic project, \nbut to do so, you will help us with few answers to the following questions.")
-	fmt.Println("\n\nWhich language would you like to initialize?")
-	var language string
+	fmt.Println("\n\nWhat is the project name you want to use?")
+	var projectName string
+	fmt.Scan(&projectName)
+	fmt.Println("Choose a number which correspond to the language you will be using:\n1.Ruby")
+	var language int
 	fmt.Scan(&language)
-	if language == "ruby" {
-		var projectName string
-		fmt.Println("Enter the name of your project: ")
-		fmt.Scan(&projectName)
-		os.Mkdir(projectName, 0755)
-	} else {
-		fmt.Println("Currently we support ruby")
+	if language != 1 {
+		for i := 0; i < 5; i++ {
+			fmt.Println("Choose a number which correspond to the language you will be using:\n1.Ruby")
+			fmt.Scan(&language)
+			if language == 1 {
+				break
+			}
+		}
+		fmt.Println("The language you chose is not supported")
 	}
 }
