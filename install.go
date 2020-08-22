67c12420c965705f7b192a90107ec5e43c332b13
descholar-ceo <emmanuellamugi@gmail.com>
2020-08-22 15:37:26 +0200

    Save the language

--- install.go
+++ install.go
@@ -5,4 +5,7 @@ import "fmt"
 func main() {
 	fmt.Println("Welcome to the Bo!ler cli utility, We will initialize your basic project, \nbut to do so, you will help us with few answers to the following questions.")
 	fmt.Println("\n\nWhich language would you like to initialize?")
+	var language string
+	fmt.Scan(&language)
+	fmt.Printf("You enetered %v", language)
 }
