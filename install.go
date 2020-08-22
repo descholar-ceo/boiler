7365fc33fd3cc91adb572eaee951c4a229f6e552
descholar-ceo <emmanuellamugi@gmail.com>
2020-08-22 15:32:24 +0200

    Add question one

--- install.go
+++ install.go
@@ -4,4 +4,5 @@ import "fmt"
 
 func main() {
 	fmt.Println("Welcome to the Bo!ler cli utility, We will initialize your basic project, \nbut to do so, you will help us with few answers to the following questions.")
+	fmt.Println("\n\nWhich language would you like to initialize?")
 }
