db2ce673cd174bc0e9e7f59800a9b66f96567c66
descholar-ceo <emmanuellamugi@gmail.com>
2020-08-22 19:05:13 +0200

    Add display values

--- main.go
+++ main.go
@@ -4,17 +4,26 @@ import (
 	"fmt"
 )
 
+var (
+	projectName   string
+	language      int
+	isRubocop     string
+	isTests       string
+	testFramework int
+	isGithub      string
+)
+
 func main() {
 	fmt.Println("Welcome to the Bo!ler cli utility, We will initialize your basic project, \nbut to do so, you will help us with few answers to the following questions.")
 
 	// project name
 	fmt.Println("\n\nWhat is the project name you want to use?")
-	var projectName string
+	// var projectName string
 	fmt.Scan(&projectName)
 
 	// choose a language
 	fmt.Println("Choose a number which correspond to the language you will be using:\n1.Ruby")
-	var language int
+	// var language int
 	fmt.Scan(&language)
 	if language != 1 {
 		for i := 0; i < 5; i++ {
@@ -28,16 +37,16 @@ func main() {
 		return
 	}
 	fmt.Println("Will you use Rubocop as a linter? Enter y for yes or any other key for no")
-	var isRubocop string
+	// var isRubocop string
 	fmt.Scan(&isRubocop)
 
 	// will you run tests?
 	fmt.Println("Will you write some unit tests for your project? Enter y for yes or any other key for no")
-	var isTests string
+	// var isTests string
 	fmt.Scan(&isTests)
 	if isTests == "y" || isTests == "Y" {
 		fmt.Println("Choose a number which corresponds to the testing framework you will be using:\n1.RSpec")
-		var testFramework int
+		// var testFramework int
 		fmt.Scan(&testFramework)
 		if testFramework != 1 {
 			for i := 0; i < 5; i++ {
@@ -53,7 +62,14 @@ func main() {
 
 	// will you use github?
 	fmt.Println("Will you use github as a collaboration tool? Enter y for yes or any other key for no")
-	var isGithub string
+	// var isGithub string
 	fmt.Scan(&isGithub)
+
+	fmt.Println("The following are your preferences, we will setup your project depending on your preferences.")
+	fmt.Printf("Project name : %v\n", projectName)
+	fmt.Printf("Language name : %v\n", language)
+	fmt.Printf("Will you write unit test? : %v\n", isTests)
+	fmt.Printf("Testing framework : %v\n", testFramework)
+	fmt.Printf("Will you use github? : %v\n", isGithub)
 
 }
