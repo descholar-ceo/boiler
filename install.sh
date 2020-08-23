1fbc6876974b7a01e25ea515d745cab4626049dd
descholar-ceo <emmanuellamugi@gmail.com>
2020-08-23 09:50:25 +0200

    Edit the cd command

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ echo "
 Welcome to Boiler package developed by descholar. This package was developed to help you make your life very easy 
 while working on the next project"
 
-mkdir $HOME/.boiler && cd $HOME/boiler && git clone https://github.com/descholar-ceo/boiler
+mkdir $HOME/.boiler && cd $HOME/.boiler && git clone https://github.com/descholar-ceo/boiler
 
 make build
 
