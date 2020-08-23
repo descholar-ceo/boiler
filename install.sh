90e6251ee5fa2b911144a60865a416ef86c836df
descholar-ceo <emmanuellamugi@gmail.com>
2020-08-23 10:55:12 +0200

    Add download files

--- install.sh
+++ install.sh
@@ -3,7 +3,22 @@ echo "
 Welcome to Boiler package developed by descholar. This package was developed to help you make your life very easy 
 while working on the next project"
 
-mkdir $HOME/.boiler && cd $HOME/.boiler && git clone https://github.com/descholar-ceo/boiler
+mkdir $HOME/.boiler $HOME/.boiler/boiler  && cd $HOME/.boiler/boiler
+
+curl -L -O https://raw.githubusercontent.com/descholar-ceo/boiler/develop/main.go
+curl -L -O https://raw.githubusercontent.com/descholar-ceo/boiler/develop/Makefile
+
+mkdir lib lib/.ruby && cd lib/.ruby
+
+curl -L -O https://raw.githubusercontent.com/descholar-ceo/boiler/develop/lib/.ruby/.rubocop.yml
+curl -L -O https://raw.githubusercontent.com/descholar-ceo/boiler/develop/lib/.ruby/README.md
+
+mkdir .github .github/workflows && cd .github/workflows
+
+curl -L -O https://raw.githubusercontent.com/descholar-ceo/boiler/develop/lib/.ruby/.github/workflows/linters.yml
+curl -L -O https://raw.githubusercontent.com/descholar-ceo/boiler/develop/lib/.ruby/.github/workflows/tests.yml
+
+cd $HOME/.boiler/boiler
 
 make build
 
