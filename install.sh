c59058103db98fca6365c95247a7d276706e825a
descholar-ceo <emmanuellamugi@gmail.com>
2020-08-23 09:46:54 +0200

    Add bash commands

--- install.sh
+++ install.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-echo "Welcome to Boiler package developed by descholar. This package was developed to help you make your life very easy 
+echo "
+Welcome to Boiler package developed by descholar. This package was developed to help you make your life very easy 
 while working on the next project"
+
+mkdir $HOME/.boiler && cd $HOME/boiler && git clone https://github.com/descholar-ceo/boiler
+
+make build
+
+sudo echo PATH=$PATH:$HOME/boiler/boiler/bin>>$HOME/.bashrc
+
+echo "Everything is set now"
