bc1467bcdd789cf64d0739567e7973b4e2d9badd
descholar-ceo <emmanuellamugi@gmail.com>
2020-08-28 14:58:52 +0200

    Remove the unecessary codes

--- main_test.go
+++ main_test.go
@@ -1,15 +1,13 @@
 package main
 
 import (
-	"fmt"
 	"os"
 	"testing"
 )
 
 func TestIsDirectoryExists(t *testing.T) {
 	os.Mkdir(getHomeDirectory()+"/.tmp", 0755)
-	answer := isDirectoryExists(getHomeDirectory() + "/descholar")
-	fmt.Printf("the value of the answer is %v\n", getHomeDirectory()+"/descholar")
+	answer := isDirectoryExists(getHomeDirectory() + "/.tmp")
 	if answer == false {
 		t.Errorf("The isDirectoryExists() should return true if the directory exists, but it returned %v\n", answer)
 	}
