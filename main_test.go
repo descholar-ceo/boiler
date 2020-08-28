312694cff3cc493e279a3200fe6239955d6c2df7
descholar-ceo <emmanuellamugi@gmail.com>
2020-08-28 14:54:54 +0200

    Add a failing tests

--- main_test.go
+++ main_test.go
@@ -1,14 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"testing"
 )
 
 func TestIsDirectoryExists(t *testing.T) {
-	os.Mkdir(".tmp", 0755)
-	answer := isDirectoryExists(".tmp")
-	if !answer {
-
+	os.Mkdir(getHomeDirectory()+"/.tmp", 0755)
+	answer := isDirectoryExists(getHomeDirectory() + "/descholar")
+	fmt.Printf("the value of the answer is %v\n", getHomeDirectory()+"/descholar")
+	if answer == false {
+		t.Errorf("The isDirectoryExists() should return true if the directory exists, but it returned %v\n", answer)
 	}
 }
