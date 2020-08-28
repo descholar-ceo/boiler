11923db079dd0fc8798afec3706a924b8c3f29b6
descholar-ceo <emmanuellamugi@gmail.com>
2020-08-28 20:47:05 +0200

    Remove the debugging instructions

--- main.go
+++ main.go
@@ -287,25 +287,12 @@ func createRubocopFile() {
 }
 
 func copy(src, dst string) (int64, error) {
-	sourceFileStat, err := os.Stat(src)
-	if err != nil {
-		return 0, err
-	}
+	source, _ := os.Open(src)
 
-	if !sourceFileStat.Mode().IsRegular() {
-		return 0, fmt.Errorf("%s is not a regular file", src)
-	}
-
-	source, err := os.Open(src)
-	if err != nil {
-		return 0, err
-	}
 	defer source.Close()
 
-	destination, err := os.Create(dst)
-	if err != nil {
-		return 0, err
-	}
+	destination, _ := os.Create(dst)
+
 	defer destination.Close()
 	nBytes, err := io.Copy(destination, source)
 	return nBytes, err
