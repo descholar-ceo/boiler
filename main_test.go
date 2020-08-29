fd9401b378752aa0fa3d08ae20f769205bfed618
descholar-ceo <emmanuellamugi@gmail.com>
2020-08-29 15:49:26 +0200

    Add test the status of language

--- main_test.go
+++ main_test.go
@@ -175,4 +175,7 @@ func TestRorBoiler(t *testing.T) {
 func TestMain(t *testing.T) {
 	language = 5
 	main()
+	if language != 0 {
+		t.Errorf("If the provided language number is not valid, the program should return and language choice should be reset to zero value, but it is still on %v", language)
+	}
 }
