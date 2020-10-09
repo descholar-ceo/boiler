254d7b3c89001904f5c1ff66f5bda05655749089
descholar-ceo <emmanuellamugi@gmail.com>
2020-10-09 17:48:58 +0200

    Asks about different vars

--- languages/ror.go
+++ languages/ror.go
@@ -4,6 +4,10 @@ import (
 	"github.com/descholar-ceo/boiler/utils"
 )
 
+// RorBoiler is a boilerplate generator for ror
 func RorBoiler() {
-	workingDir := utils.AskWorkingDirectory
+	workingDir := utils.AskWorkingDirectory()
+	isGithub := utils.AskGithub()
+	isRubocop := utils.AskRubocop()
+	database := utils.AskDatabase()
 }
