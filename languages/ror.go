6c4d942f223c53b15062cc8bf316875b8bdc2abb
descholar-ceo <emmanuellamugi@gmail.com>
2020-10-09 17:50:01 +0200

    Ask the project name

--- languages/ror.go
+++ languages/ror.go
@@ -7,6 +7,7 @@ import (
 // RorBoiler is a boilerplate generator for ror
 func RorBoiler() {
 	workingDir := utils.AskWorkingDirectory()
+	projectName := utils.AskProjectName()
 	isGithub := utils.AskGithub()
 	isRubocop := utils.AskRubocop()
 	database := utils.AskDatabase()
