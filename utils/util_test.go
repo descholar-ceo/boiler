22930ed06d399945411b61ef1a78252cc42e877f
descholar-ceo <emmanuellamugi@gmail.com>
2020-10-18 12:59:42 +0200

    Add test jdbc

--- utils/util_test.go
+++ utils/util_test.go
@@ -343,6 +343,26 @@ func TestAskDatabaseOption10(t *testing.T) {
 		t.Errorf("The AskDatabase is not working!")
 	}
 }
+func TestAskDatabaseOption11(t *testing.T) {
+	in, err := ioutil.TempFile("", "")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer in.Close()
+
+	_, err = io.WriteString(in, "11")
+	if err != nil {
+		t.Fatal(err)
+	}
+	_, err = in.Seek(0, os.SEEK_SET)
+	if err != nil {
+		t.Fatal(err)
+	}
+	workingDir := AskDatabase(in)
+	if workingDir != "jdbc" {
+		t.Errorf("The AskDatabase is not working!")
+	}
+}
 
 func TestAskProjectName(t *testing.T) {
 	in, err := ioutil.TempFile("", "")
