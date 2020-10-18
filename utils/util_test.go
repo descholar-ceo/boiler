ba66bb375764c8fce2844aa0a13974830a507eb7
descholar-ceo <emmanuellamugi@gmail.com>
2020-10-18 13:00:32 +0200

    Add test default

--- utils/util_test.go
+++ utils/util_test.go
@@ -363,6 +363,26 @@ func TestAskDatabaseOption11(t *testing.T) {
 		t.Errorf("The AskDatabase is not working!")
 	}
 }
+func TestAskDatabaseOptionDefault(t *testing.T) {
+	in, err := ioutil.TempFile("", "")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer in.Close()
+
+	_, err = io.WriteString(in, "11000")
+	if err != nil {
+		t.Fatal(err)
+	}
+	_, err = in.Seek(0, os.SEEK_SET)
+	if err != nil {
+		t.Fatal(err)
+	}
+	workingDir := AskDatabase(in)
+	if workingDir != "sqlite3" {
+		t.Errorf("The AskDatabase is not working!")
+	}
+}
 
 func TestAskProjectName(t *testing.T) {
 	in, err := ioutil.TempFile("", "")
