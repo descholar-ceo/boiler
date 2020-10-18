28490589d0fe72372cd831ed52d1c2f2058a8de2
descholar-ceo <emmanuellamugi@gmail.com>
2020-10-18 17:16:40 +0200

    Fix ask project name as well

--- utils/utils.go
+++ utils/utils.go
@@ -103,13 +103,13 @@ func AskDatabase(in *os.File) string {
 
 // AskProjectName function
 func AskProjectName(in *os.File) string {
-	in = generateStdin(in)
+	// in = generateStdin(in)
 	var projectName string
-	fmt.Println("\n\nWhat is the project name you want to use?")
-	if _, err := fmt.Fscanf(in, "%s", &projectName); err != nil {
-		panic(err)
-	}
-	return projectName
+	// fmt.Println("\n\nWhat is the project name you want to use?")
+	// if _, err := fmt.Fscanf(in, "%s", &projectName); err != nil {
+	// 	panic(err)
+	// }
+	return askBasicQuestions(in, projectName, "\n\nWhat is the project name you want to use?")
 }
 
 // Copy function to copy files
