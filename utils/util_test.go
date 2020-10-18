5812cd36165ba6573e6021ad132e1f6c77a6b128
descholar-ceo <emmanuellamugi@gmail.com>
2020-10-18 12:55:04 +0200

    Add test oracle

--- utils/util_test.go
+++ utils/util_test.go
@@ -203,6 +203,26 @@ func TestAskDatabaseOption3(t *testing.T) {
 		t.Errorf("The AskDatabase is not working!")
 	}
 }
+func TestAskDatabaseOption4(t *testing.T) {
+	in, err := ioutil.TempFile("", "")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer in.Close()
+
+	_, err = io.WriteString(in, "4")
+	if err != nil {
+		t.Fatal(err)
+	}
+	_, err = in.Seek(0, os.SEEK_SET)
+	if err != nil {
+		t.Fatal(err)
+	}
+	workingDir := AskDatabase(in)
+	if workingDir != "oracle" {
+		t.Errorf("The AskDatabase is not working!")
+	}
+}
 
 func TestAskProjectName(t *testing.T) {
 	in, err := ioutil.TempFile("", "")
