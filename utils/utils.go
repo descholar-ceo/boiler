963ec5620da0acba7554be5bae357d8f096880ce
descholar-ceo <emmanuellamugi@gmail.com>
2020-10-19 21:40:07 +0200

    Eliminate empty lines

--- utils/utils.go
+++ utils/utils.go
@@ -21,11 +21,9 @@ func askBasicQuestions(in *os.File, variable string, textToAsk string) string {
 	in = generateStdin(in)
 	fmt.Println(textToAsk)
 	if _, err := fmt.Fscanf(in, "%s", &variable); err != nil {
-		log.Fatal(`
-|=====================Error message===================|
-||    Empty response, please enter a value!          ||
-|=====================================================|
-`)
+		log.Fatal(`|=====================Error message===================|
+							 ||    Empty response, please enter a value!          ||
+							 |=====================================================|`)
 	}
 	return variable
 }
